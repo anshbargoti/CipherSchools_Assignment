5e6af07c0f7a1754a54c42f08075a1bb4d3ca57a
ansh <anshbargoti@gmail.com>
2021-02-17 16:06:47 +0530

    Day3

---
+++ LinkedLists/ReverseLinkedlist(Iterative).cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+//#define int long long
+// const int MOD = 1000000007;
+
+
+
+struct Node 
+{
+    int data;
+    Node *next;
+    Node (int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+struct Node *reversell(Node *head)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next_val = NULL;
+    while (curr != NULL)
+    {
+        next_val = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next_val;
+    }
+    return prev;
+}
+
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr -> data;
+        cout << "->";
+        if (curr -> next == NULL)
+            cout << "NULL";
+        curr = curr -> next;
+    }
+}
+
+void striker()
+{
+    int n;
+    cin >> n;
+    Node *root = NULL, *tail = NULL;
+    int firstval;
+    cin >> firstval;
+    root = new Node(firstval);
+    tail = root;
+    for (int i = 1; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        tail -> next = new Node(x);
+        tail = tail -> next;
+    }
+    Node *head = reversell(root);
+    printlist(head);
+}
+
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        // cout << "#Case: " << i + 1 << endl;
+        striker();
+        cout << endl;
+    }
+    return 0;
+}
---
+++ LinkedLists/ReverseLinkedlist(Recursive).cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+//#define int long long
+// const int MOD = 1000000007;
+
+
+
+struct Node 
+{
+    int data;
+    Node *next;
+    Node (int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void reversell(Node **head, Node *curr, Node *prev)
+{
+    if (curr -> next == NULL)
+    {
+        *head = curr;
+        curr -> next = prev;
+        return;
+    }
+
+    Node *next_val = curr -> next;
+    curr -> next = prev;
+    reversell(head, next_val, curr);
+}
+
+void reverse(Node** head)
+{
+    if (!head)
+        return;
+    reversell(head, *head, NULL);
+}
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr -> data;
+        cout << "->";
+        if (curr -> next == NULL)
+            cout << "NULL";
+        curr = curr -> next;
+    }
+}
+
+void striker()
+{
+    int n;
+    cin >> n;
+    Node *root = NULL, *tail = NULL;
+    int firstval;
+    cin >> firstval;
+    root = new Node(firstval);
+    tail = root;
+    for (int i = 1; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        tail -> next = new Node(x);
+        tail = tail -> next;
+    }
+    reverse(&root);
+    printlist(root);
+}
+
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        // cout << "#Case: " << i + 1 << endl;
+        striker();
+        cout << endl;
+    }
+    return 0;
+}
